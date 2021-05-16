4018e8d085ac899ff948f469a3c9f1d8777dedc4
Mglt_b <20703341+Mglt-b@users.noreply.github.com>
2021-05-16 14:27:01 +0200

    Add files via upload

--- Arena.py
+++ Arena.py
@@ -50,93 +50,16 @@ while 1>0:
                     pyautogui.moveTo(b_rival_pos[0], b_rival_pos[1])
                     print("on est en bas de la liste rival, on continue")
                     time.sleep(1)
-                    pyautogui.scroll(-100)
+                    pyautogui.scroll(-120)
                     random_sleep()
                 
 
 
             if nb_ailes > 0:
 
-                x_ailes_count_pos_list = []
-                y_ailes_count_pos_list = []
+                debut_liste = 1
+                do_wings(is_rival,ailes_count_pos,debut_liste)
 
-                for (x_ailes_count, y_ailes_count) in ailes_count_pos : 
-
-
-                    next_fight_pos = [x_ailes_count,y_ailes_count]
-
-                    random_sleep()
-                    pyautogui.click(next_fight_pos[0], next_fight_pos[1])
-                    random_sleep()
-                    #check si monodef
-
-                    solo_def_pos = imagesearch("./img/solo_def.png")
-                    random_sleep()
-                    solo_def2_pos = imagesearch("./img/solo_def2.png")
-
-                    if is_rival == 1 or solo_def_pos[0] != -1 or solo_def2_pos[0] != -1:
-
-                        print("Il y a un seul mob en def, ou bien rival.")
-
-                    
-                        random_sleep()       
-
-                        start_battle_pos = imagesearch_loop("./img/start_battle.png",1)
-
-                        if start_battle_pos[0] != -1:
-
-                            random_sleep()  
-                            pyautogui.click(start_battle_pos[0], start_battle_pos[1])
-
-                            #puis lancer mode auto
-
-                            auto_mode_pos = imagesearch_loop("./img/auto_mode.png",5)
-
-                            if auto_mode_pos[0] != -1:
-
-                                random_sleep()  
-                                pyautogui.click(auto_mode_pos[0], auto_mode_pos[1]) 
-
-                                end_fight_pos = imagesearch_loop("./img/end_fight.png",5)
-
-                                if end_fight_pos[0] != -1:
-
-                                    random_sleep()  
-                                    pyautogui.click(end_fight_pos[0], end_fight_pos[1])
-                                    random_sleep()  
-                                    
-                                    if is_rival == 0:
-                                        end_fight2_pos = imagesearch_loop("./img/end_fight2.png",1)
-
-                                        if end_fight2_pos[0] != -1:
-
-                                            random_sleep()  
-                                            pyautogui.click(end_fight2_pos[0], end_fight2_pos[1])
-           
-                    else:
-                        print("Il semble y avoir une vraie def, def suivante.")
-                        exit_menu_fight_pos = imagesearch_loop("./img/exit_menu_fight.png",1)
-
-                        if exit_menu_fight_pos[0] != -1:
-
-                            random_sleep()  
-                            pyautogui.click(exit_menu_fight_pos[0], exit_menu_fight_pos[1])
-                        else:
-                            exit_menu_fight2_pos = imagesearch_loop("./img/exit_menu_fight2.png",1)
-
-                            if exit_menu_fight2_pos[0] != -1:
-
-                                random_sleep()  
-                                pyautogui.click(exit_menu_fight2_pos[0], exit_menu_fight2_pos[1]) 
-                if is_rival == 0:
-                    refresh_list()
-                else :
-                    random_sleep()
-                    match_up_pos = imagesearch("./img/match_up.png")
-                    if match_up_pos[0] != -1:
-                        print("On retourne dans liste arene.")
-                        random_sleep()
-                        pyautogui.click(match_up_pos[0],match_up_pos[1])
 
 
 
